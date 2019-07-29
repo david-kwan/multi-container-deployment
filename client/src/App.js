eb6d6a6ac48db89485da338b48712da84fb1bd60
David Kwan <daves.kwan@gmail.com>
2019-07-28 23:11:06 -0400

    updated page header

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,18 @@ import Fib from './Fib';
 class App extends Component {
   render() {
     return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
-        </header>
-        <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1>David's blog</h1>
+            <Link to="/">Home</Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
         </div>
-      </div>
-    </Router>
-      
+      </Router>
     );
   }
 }
